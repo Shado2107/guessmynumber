2ec62e2a9b98149e4412ae76e4bc42b4d1cca4e9
Shado2107 <pd39almeida@outlook.com>
2023-03-24 17:28:11 +0000

    update

--- src/App.js
+++ src/App.js
@@ -7,16 +7,28 @@ const App = () => {
   const [answer, setAnswer] = useState(0)
   const [message, setMessage] = useState('Guess a number between 1 and 100');
   const [score, setScore] = useState(0);
+  const [playerName, setPlayerName] = useState('');
+  const [highScores, setHighScores] = useState([]);
 
   const handleInputChange = (event) => {
     setGuess(parseInt(event.target.value));
   };
 
+  const handleNameInputChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
     if (guess === secretNumber) {
+      const newScore = {
+        name: playerName,
+        score: score + 10
+      };
+      setHighScores([...highScores, newScore]);
       setMessage(`Congratulations! You guessed the number ${secretNumber}!`);
-      setScore(score + 10);
+      // setScore(score + 10);
+      setScore(0);
       setAnswer(secretNumber);
       // setSecretNumber(Math.floor(Math.random() * 100) + 1);
     } else if (guess < secretNumber) {
@@ -47,18 +59,39 @@ const App = () => {
       <main>
         <section className='left'>
           <form onSubmit={handleFormSubmit}>
-            <input className="guess" type="number" value={guess} onChange={handleInputChange} />
+            {/* <input className="guess" type="number" value={guess} onChange={handleInputChange} /> */}
+            <label htmlFor="guess">Guess:</label>
+            <input type="number" id="guess" name="guess" value={guess} onChange={handleInputChange} />
+            <label htmlFor="player-name">Name:</label>
+            <input type="text" id="player-name" name="player-name" value={playerName} onChange={handleNameInputChange} />
+            {/* <button type="submit">Submit</button> */}
             <button  className="btn check" >Submit Guess</button>
           </form>
         </section>
         <section className='right'>
         <p className="message">{message}</p>
           <p className="label-score">
-            💯 Score: <span className="score">20</span>
+            💯 Score: <span className="score"> {score} </span>
           </p>
-          <p className="label-highscore">
-            🥇 Highscore: <span className="highscore"> {score} </span>
-          </p>
+          <h2 className="label-highscore">🥇 Highscore: </h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              {highScores.sort((a, b) => b.score - a.score).map((score, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{score.name}</td>
+                  <td>{score.score}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </section>
       </main>
     </>
